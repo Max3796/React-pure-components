0b83b83625f4ad149f179b0f2b5e45dcfd3b3484
Max3796 <gurpreet.singh@kalvium.community>
2024-02-08 20:21:17 +0530

    Done

---
+++ pureComponents/src/App.jsx
@@ -0,0 +1,17 @@
+import { PureComponent, useState } from 'react'
+import './App.css'
+import SimpleComponent from './components/SimpleComponent'
+import Pure from './components/PureComponent'
+
+function App() {
+ 
+
+  return (
+    <>
+      <SimpleComponent />
+      <Pure />
+    </>
+  )
+}
+
+export default App
---
+++ pureComponents/src/components/PureComponent.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+export default class Pure extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            count:0,
+            toggle: true
+        }
+    }
+
+    changeToggle = () =>{
+        this.setState({
+            toggle: !this.state.toggle
+        })
+    }
+    increaseCount = () =>{
+        if(this.state.toggle){
+            this.setState({
+                count: this.state.count + 1
+            })
+        }else{
+            this.setState({
+                count: this.state.count
+            })
+        }
+    }
+
+  render() {
+    console.warn("This is a pure component")
+    return (
+      <>
+    <h1>Pure Counter</h1>
+    <h3>{this.state.count}</h3>
+    <button onClick={this.changeToggle} style={{ backgroundColor: this.state.toggle ? "green" : "red" }}>Toggle</button>
+
+    <button style={{marginLeft: "10px"}} onClick={this.increaseCount}>+</button>
+
+      </>
+    )
+  }
+}
